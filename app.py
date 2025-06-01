5bc4006205f03f53abdbc33c96dbc091dfab0a4f
thepm-cpu <thepm002@gmail.com>
2025-06-01 17:28:40 +0100

    Update homepage message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask in Docker!"
+    return "Hey future Chibuzor, this is our first website!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
