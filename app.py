99e9f2810b98cf7b401edf177633b6f4530799ab
thepm-cpu <thepm002@gmail.com>
2025-06-04 17:58:10 +0100

    updated the app

--- app.py
+++ app.py
@@ -17,9 +17,11 @@ class Message(db.Model):
     content = db.Column(db.String(200), nullable=False)
 
 # This ensures database + table is created before first request
-@app.before_first_request
-def create_tables():
-    db.create_all()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host="0.0.0.0")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
